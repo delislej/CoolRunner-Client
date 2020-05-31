1faea4f51e2ef5c1561a174bee6a034450bef2ca
ninecats <delislej@gmail.com>
2020-05-30 22:50:55 -0700

    intital commit

---
+++ components/PlannerMap.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class PlannerMap extends Component {
+    state = {}
+
+
+
+
+  render () {return (
+      <text>hello</text>
+    )
+  }
+}
+
+export default PlannerMap
---
+++ screens/PlannerScreen.js
@@ -0,0 +1,153 @@
+
+import React, { useState, useEffect } from 'react'
+
+import MapView from 'react-native-maps'
+import { Platform, Button, Alert, StyleSheet, Text, View, Dimensions } from 'react-native'
+import Constants from 'expo-constants'
+import * as Location from 'expo-location'
+
+export default function PlannerScreen () {
+
+  const [location, setLocation] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  useEffect(() => {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      setErrorMsg(
+        'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+      )
+    } else {
+      (async () => {
+        const { status } = await Location.requestPermissionsAsync()
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied')
+        }
+
+        const location = await Location.getCurrentPositionAsync({})
+        setLocation(location)
+      })()
+    }
+  })
+
+  let text = 'Waiting..'
+  if (errorMsg) {
+    text = errorMsg
+  } else if (location) {
+    text = JSON.stringify(location)
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.tabBarInfoText}>This is a tab bar.</Text>
+        <MapView style={styles.mapStyle} />
+        <View
+          style={{
+            position: 'absolute', // use absolute position to show button on top of the map
+            top: '90%', // for center align
+            left: '40%' // for align to right
+          }}
+        >
+
+          <Button
+            title='Press me'
+            onPress={() => Alert.alert(text)}
+          />
+        </View>
+
+        <View style={[styles.codeHighlightContainer, styles.navigationFilename]} />
+      </View>
+    </View>
+  )
+}
+
+PlannerScreen.navigationOptions = {
+  header: null
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center'
+  },
+  contentContainer: {
+    paddingTop: 30
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50
+  },
+  homeScreenFilename: {
+    marginVertical: 7
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)'
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center'
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 20
+      }
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center'
+  },
+  navigationFilename: {
+    marginTop: 5
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center'
+  },
+  helpLink: {
+    paddingVertical: 15
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7'
+  }
+})
